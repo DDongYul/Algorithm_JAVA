21dec35575b9d3558265c8b90ba7d285004caf0b
이동열(Lee Dong Yul) <comeme0101@naver.com>
2024-01-26 00:13:15 +0900

    [Gold V] Title: 트리, Time: 136 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.java
+++ 백준/Gold/1068. 트리/트리.java
@@ -3,6 +3,14 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
+/**
+ * 입력 순서대로 트리에 add하면 입력값 중 부모노드가 없는 상황 발생 (아래는 예시)
+ * 5
+ * 1 -1 0 1 1
+ * 3
+ * 출력값: 2
+ */
+
 public class Main {
 
     static class Node {
@@ -44,16 +52,15 @@ public class Main {
             return null;
         }
 
+        //트리에 삽입 (부모 노드가 트리에 없으면 예외발생)
         public void add(int parentIndex, int index){
             Node parent = search(parentIndex);
-            if(parent == null){
-
-            }
             Node child = new Node(index);
             parent.children.add(child);
             child.Parent = parent;
         }
 
+        //부모 노드의 자식리스트에서 지우기
         public void remove(int index) {
             Node searchNode = search(index);
             if(searchNode == root){
@@ -65,7 +72,7 @@ public class Main {
             }
         }
 
-        //root 부터 시작하여 Stack에 넣으며 탐색 시작, 자식이 없으면 리프노드로 카운트, 있으면 스택에 넣어줌
+        //root 부터 시작하여 Stack 이용하여 DFS
         public int countLeap() {
             if(root == null){
                 return 0;
@@ -87,7 +94,7 @@ public class Main {
         }
     }
 
-    //(x,y) 편의를 위해 만듬 
+    //(x,y) 편의를 위해 만든 클래스
     static class Pair{
         int x;
         int y;
@@ -104,25 +111,25 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         Tree tree = new Tree();
-        
-        Deque<Pair> deque = new LinkedList<>(); //부모가 트리에 있는 노드만 트리에 넣기 위해 사용
-        int[] visited = new int[N];    //부모가 만들어져있는지 체크하기 위한 배열
+
+        Deque<Pair> deque = new LinkedList<>(); //부모가 트리에 존재하는 경우만 트리에 넣기 위해 사용
+        int[] hasParent = new int[N];    //부모가 만들어져있는지 체크하기 위한 배열
 
         for (int i = 0; i < N; i++) {
             int n = Integer.parseInt(st.nextToken());
             if(n == -1){    //루트 노드일때
-                visited[i] = 1;
+                hasParent[i] = 1;
                 tree.addRoot(i);
                 continue;
             }
-            deque.addLast(new Pair(n,i)); //트리에 바로 add하지 않고 deque에 넣음
+            deque.addLast(new Pair(n,i)); //(부모인덱스, 본인인덱스), 트리에 삽입하지 않고 우선 deque에 넣음
         }
-        
+
         while (!deque.isEmpty()){
             Pair pair = deque.pollFirst();
-            if (visited[pair.x] == 1){    //부모 노드가 트리에 있을 떼
+            if (hasParent[pair.x] == 1){    //부모 노드가 트리에 있을 떼
                 tree.add(pair.x, pair.y);
-                visited[pair.y] = 1;
+                hasParent[pair.y] = 1;
             }
             else{
                 deque.addLast(pair);    //부모 노드가 아직 없으면 후순위로 미룸
