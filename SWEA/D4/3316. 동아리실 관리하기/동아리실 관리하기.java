679be80080ac4432451ef09ec4b8f70035c4af39
이동열(Lee Dong Yul) <comeme0101@naver.com>
2024-01-29 14:45:34 +0900

    [D4] Title: 동아리실 관리하기, Time: 459 ms, Memory: 91,448 KB -BaekjoonHub

---
+++ SWEA/D4/3316. 동아리실 관리하기/동아리실 관리하기.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+
+	//
+	static int getBit(char ch) {
+		switch (ch) {
+		case 'A':
+			return 1;
+		case 'B':
+			return 2;
+		case 'C':
+			return 4;
+		case 'D':
+			return 8;
+		}
+		return 0;
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+
+		for (int t = 0; t < T; t++) {
+			String str = br.readLine();
+			int N = str.length();
+			int[][] dp = new int[N + 1][16];
+			int A = getBit('A');
+			dp[0][A] = 1;
+
+			for (int day = 1; day <= N; day++) {
+				int hasKey = getBit(str.charAt(day-1));
+				List<Integer> today = new ArrayList();
+
+				for (int i = 1; i < 16; i++) { // key를 가지고 있는 사람이 반드시 참여하는 경우
+					if ((hasKey & i) != 0) {
+						today.add(i);
+					}
+				}
+
+				for (int i = 1; i < 16; i++) {
+					for (Integer td : today) {
+						if ((i & td) != 0) {
+							dp[day][td] += dp[day - 1][i]; 
+							dp[day][td]%= 1000000007;
+						}
+					}
+				}
+
+			}
+
+			int rst = 0;
+			for (int i = 0; i < 16; i++) {
+				rst+=dp[N][i]; 
+				rst%=1000000007;
+			}
+			StringBuilder sb = new StringBuilder("#").append(t+1).append(" ").append(rst);
+			System.out.println(sb);
+		}
+
+
+	}
+}
