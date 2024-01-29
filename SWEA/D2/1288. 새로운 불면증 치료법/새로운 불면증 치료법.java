c5b896da9adaf0700bf67d69466ceea9fb967050
이동열(Lee Dong Yul) <comeme0101@naver.com>
2024-01-29 14:46:32 +0900

    [D2] Title: 새로운 불면증 치료법, Time: 115 ms, Memory: 18,256 KB -BaekjoonHub

---
+++ SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Solution
+{
+	public static int sol(int num) {
+		int m = num;
+		int bitmap = 0;
+		while (true) {
+			int temp = num;
+			while (temp > 0) {
+				int idx = temp % 10;
+				int bit = 1<<idx;
+				bitmap = bitmap|bit;
+				temp /= 10;
+			}
+			if (Integer.bitCount(bitmap) == 10) {
+				break;
+			}
+			num += m;
+		}
+		return num;
+	}
+	
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for (int i = 0; i < T; i++) {
+			StringBuilder sb = new StringBuilder();
+			int num = Integer.parseInt(br.readLine());
+			sb.append("#").append(i+1).append(" ").append(sol(num));
+			System.out.println(sb.toString());
+		}
+	}
+}
