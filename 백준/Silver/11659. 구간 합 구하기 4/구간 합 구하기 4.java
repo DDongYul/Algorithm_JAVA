983fba41b318255ad2fc3d87500ff809b60108ec
이동열(Lee Dong Yul) <comeme0101@naver.com>
2024-01-31 14:28:20 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 2096 ms, Memory: 71056 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[] arr = new int[N];
+		st = new StringTokenizer(br.readLine());
+		arr[0] = Integer.parseInt(st.nextToken());
+		for(int i = 1; i<N; i++) {
+			arr[i] = arr[i-1] + Integer.parseInt(st.nextToken());
+		}
+		for(int i = 0; i<M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			if(a==1) {
+				System.out.println(arr[b-1]);				
+			}
+			else {
+				System.out.println(arr[b-1] - arr[a-2]);	
+			}
+		}
+	}
+}
