b557fc169afdfe6e16765c66018d57d3136a92ae
이동열(Lee Dong Yul) <comeme0101@naver.com>
2024-02-05 16:31:37 +0900

    [Gold III] Title: 정복자, Time: 364 ms, Memory: 26780 KB -BaekjoonHub

--- 백준/Gold/14950. 정복자/정복자.java
+++ 백준/Gold/14950. 정복자/정복자.java
@@ -2,23 +2,22 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 /**
- * prim 알고리즘으로 풀기 각 라운드에서 PQ로 꺼내가며 진행
- *
+ * prim 알고리즘이용! 
+ * 각 라운드에서 현재 갈 수 있는 간선 중 비용이 가장 작은 것을 PQ로 꺼내가며 진행
+ * 이 간선의 도착 지점이 방문 안한 정점이면 그룹에 추가(방문 배열 체크)
  */
 public class Main {
 
+	//각 정점을 표현
 	static class Node{
 		int index;
-		List<Edge> edge;
+		List<Edge> edge; //해당 정점과 연결된 간선
 
 		public Node(int index) {
 			this.index = index;
@@ -27,8 +26,8 @@ public class Main {
 	}
 	
 	static class Edge{
-		int end;
-		int cost;
+		int end;	//간선의 도착지점
+		int cost;	//간선의 비용
 		
 		public Edge(int end, int cost) {
 			this.end = end;
@@ -44,6 +43,7 @@ public class Main {
 		int M = Integer.parseInt(st.nextToken());
 		int t = Integer.parseInt(st.nextToken());
 
+		//정점 초기화
 		Node[] nodes = new Node[N + 1];
 		for (int i = 1; i <= N; i++) {
 			nodes[i] = new Node(i);
@@ -54,10 +54,12 @@ public class Main {
 			int s = Integer.parseInt(st.nextToken());
 			int e = Integer.parseInt(st.nextToken());
 			int c = Integer.parseInt(st.nextToken());
+			//양방향 매핑
 			nodes[s].edge.add(new Edge(e,c));
 			nodes[e].edge.add(new Edge(s,c));
 		}
 		
+		//Union find로 싸이클 찾기 대신 방문으로 체크
 		boolean[] visited = new boolean[N+1];
 		visited[1] = true;
 		
@@ -68,6 +70,7 @@ public class Main {
 			}
 		});
 		
+		//1번 정점 간선 넣고 시작
 		for (Edge edge : nodes[1].edge) {
 			pq.add(edge);
 		}
