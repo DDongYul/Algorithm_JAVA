2225c103fd5285b0f3b5b2a77966a9769b556a9f
이동열(Lee Dong Yul) <comeme0101@naver.com>
2024-02-15 10:46:03 +0900

    [Gold II] Title: 빵집, Time: 324 ms, Memory: 42084 KB -BaekjoonHub

--- 백준/Gold II/3109. 빵집/빵집.java
+++ 백준/Gold II/3109. 빵집/빵집.java
@@ -5,8 +5,10 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /**
- *	그리디 적용 -> 왼쪽 위부터 탐색시작하고 오른쪽 위로 갈 수 있으면 무조건 위로 가는게 좋을듯
- *	실패하면 경로 버려야한다.
+ *	그리디 적용 -> 왼쪽 위부터 탐색시작하고 오른쪽 위로 갈 수 있으면 무조건 위로 간다.
+ *	실패하면 경로 버려야한다. -> 그럴 필요가 없다. 어차피 위에서 못가는 경로면 아래 출발도 못가는 경로
+ *	따라서 도착 못하더라도 방문처리 해버려도 된다.
+ *	만약 우선순위 방향으로 탐색을 성공했으면 나머지 방향은 탐색 x -> flag 사용 
  */
 public class Main {
 	
@@ -15,11 +17,11 @@ public class Main {
 	static char[][] arr;
 	static boolean[][] visited;
 	
-	static int DFS(int x, int y) {
-		int flag = 0;
+	static boolean DFS(int x, int y) {
+		boolean flag = false;
 		if(y == C-1) {
 			cnt+=1;
-			return 1;
+			return true;
 		}
 		
 		arr[x][y] = 'x';
@@ -27,10 +29,10 @@ public class Main {
 		if(x-1>=0 && arr[x-1][y+1] == '.') {
 			flag = DFS(x-1,y+1);
 		}
-		if(flag == 0 && arr[x][y+1] == '.'){
+		if(!flag && arr[x][y+1] == '.'){
 			flag = DFS(x,y+1);
 		}
-		if(flag == 0 && x+1<R && arr[x+1][y+1] == '.'){
+		if(!flag && x+1<R && arr[x+1][y+1] == '.'){
 			flag = DFS(x+1,y+1);
 		}
 		return flag;
