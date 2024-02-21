328e9152c7ffb16ec2910e336600d437c663c52a
이동열(Lee Dong Yul) <comeme0101@naver.com>
2024-02-21 11:00:45 +0900

    [Gold V] Title: 치킨 배달, Time: 264 ms, Memory: 17168 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,9 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Deque;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -12,64 +10,45 @@ import java.util.StringTokenizer;
  */
 public class Main {
 
-    static int N, M;
-    static int[][] arr;
-    static List<Point> chicken;
-    static List<Point> house;
-    static int answer = Integer.MAX_VALUE;
-    static int[] dx = {0, 0, 1, -1};
-    static int[] dy = {1, -1, 0, 0};
-
-    static class Point{
+    static class Point {
         int x;
         int y;
-        int d;
 
-        public Point(int x, int y, int d) {
+        public Point(int x, int y) {
             this.x = x;
             this.y = y;
-            this.d = d;
         }
     }
 
-    static int BFS(Point start) {
-        Deque<Point> deque = new ArrayDeque<>();
-        deque.add(start);
-        boolean[][] visited = new boolean[N][N];
-        visited[start.x][start.y] = true;
-        while (!deque.isEmpty()) {
-            Point curr = deque.removeFirst();
-            int x = curr.x;
-            int y = curr.y;
-            int d = curr.d;
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                if (0 <= nx && nx < N && 0 <= ny && ny < N && !visited[nx][ny]) {
-                    if(arr[nx][ny] == 2) {
-                        return d+1;
-                    }
-                    visited[nx][ny] = true;
-                    deque.add(new Point(nx, ny, d+1));
-                }
-            }
-        }
-        return 0;
-    }
+    static int N, M;
+    static int[][] arr;
+    static List<Point> chicken = new ArrayList<>(); //치킨 집 좌표 리스트
+    static List<Point> house = new ArrayList<>();   //일반 집 좌표 리스트
+    static boolean[] selected; //선택되는 치킨집
+    static int answer = Integer.MAX_VALUE;
 
     static void sol(int depth, int curr) {
-        if(depth == M){
-            int sum = 0;
-            for (Point point : house) {
-                sum += BFS(point);
+        if (depth == M) {
+            int total = 0;
+            for (Point house : house) {
+                int temp = Integer.MAX_VALUE;
+                for (int i = 0; i < chicken.size(); i++) {
+                    if (selected[i]) {
+                        temp = Math.min(temp, Math.abs(house.x - chicken.get(i).x) + Math.abs(
+                            house.y - chicken.get(i).y));
+                    }
+                }
+                total += temp;
             }
-            answer = Math.min(answer, sum);
+            answer = Math.min(answer, total);
             return;
         }
         for (int i = curr+1; i < chicken.size(); i++) {
-            arr[chicken.get(i).x][chicken.get(i).y] = 2;
-            sol(depth + 1, i);
-            arr[chicken.get(i).x][chicken.get(i).y] = 0;
+            if (!selected[i]) {
+                selected[i] = true;
+                sol(depth + 1, i);
+                selected[i] = false;
+            }
         }
     }
 
@@ -81,24 +60,21 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        chicken = new ArrayList<>();
-        house = new ArrayList<>();
-
         arr = new int[N][N];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
-                if(arr[i][j] == 1){
-                    house.add(new Point(i, j,0));
+                if (arr[i][j] == 1) {
+                    house.add(new Point(i, j));
                 }
-                if(arr[i][j] == 2){
-                    chicken.add(new Point(i, j,0));
-                    arr[i][j] = 0;
+                if (arr[i][j] == 2) {
+                    chicken.add(new Point(i, j));
                 }
             }
         }
 
+        selected = new boolean[chicken.size()];
         sol(0,-1);
         System.out.println(answer);
     }
