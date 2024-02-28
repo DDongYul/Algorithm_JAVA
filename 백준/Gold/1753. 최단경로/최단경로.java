5370596859788872ce3eccfb4d8d40cc715fb243
이동열(Lee Dong Yul) <comeme0101@naver.com>
2024-02-28 10:06:33 +0900

    [Gold IV] Title: 최단경로, Time: 664 ms, Memory: 116856 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -9,76 +9,80 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static class Edge implements Comparable {
+	static class Edge implements Comparable<Edge> {
+		int end;
+		int cost;
 
-        int e;
-        int cost;
+		public Edge(int end, int cost) {
+			this.end = end;
+			this.cost = cost;
+		}
 
-        public Edge(int e, int cost) {
-            this.e = e;
-            this.cost = cost;
-        }
+		@Override
+		public int compareTo(Edge e) {
+			return this.cost - e.cost;
+		}
 
-        @Override
-        public int compareTo(Object o) {
-            Edge e = (Edge) o;
-            return this.cost - e.cost;
-        }
-    }
+	}
 
-    static int V, E, K;
-    static List<Edge>[] edges;
-    static int[] dist;
-
-    static void dijkstra() {
-        PriorityQueue<Edge> pq = new PriorityQueue<>();
-        dist[K] = 0;
-        pq.add(new Edge(K,0));
-        while (!pq.isEmpty()) {
-            Edge curr = pq.poll();
-            if(curr.cost> dist[curr.e]){
-                continue;
-            }
-            for (Edge edge : edges[curr.e]) {
-                if(dist[edge.e] > curr.cost+ edge.cost){
-                    dist[edge.e] = curr.cost+ edge.cost;
-                    pq.add(new Edge(edge.e, curr.cost+ edge.cost));
-                }
-            }
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int INF = 1_000_000;
-        V = Integer.parseInt(st.nextToken());
-        E = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(br.readLine());
-        edges = new List[V + 1];
-        for (int i = 0; i <= V; i++) {
-            edges[i] = new ArrayList<>();
-        }
-        dist = new int[V + 1];
-        Arrays.fill(dist, INF);
-        for (int i = 0; i < E; i++) {
-            st = new StringTokenizer(br.readLine());
-            int s = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-            edges[s].add(new Edge(e, c));
-        }
-        dijkstra();
-        for (int i = 1; i <= V; i++) {
-            if (dist[i] == INF) {
-                sb.append("INF").append("\n");
-            } else {
-                sb.append(dist[i]).append("\n");
-            }
-        }
-        System.out.println(sb);
-
-    }
-
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int V = Integer.parseInt(st.nextToken());
+		int E = Integer.parseInt(st.nextToken());
+		
+		int K = Integer.parseInt(br.readLine());
+		
+		List<Edge>[] edges = new List[V+1];
+		for(int i = 1; i<=V; i++) {
+			edges[i] = new ArrayList<Edge>();
+		}
+		
+		for(int i = 0; i<E; i++) {
+			st = new StringTokenizer(br.readLine());
+			int s = Integer.parseInt(st.nextToken());
+			int e = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			edges[s].add(new Edge(e,c));			
+		}
+		
+		int[] dist = new int[V+1];
+		int INF = Integer.MAX_VALUE;
+		for(int i = 0; i<=V; i++) {
+			dist[i] = INF;
+		}
+		
+		PriorityQueue<Edge> pq = new PriorityQueue<>();
+		pq.add(new Edge(K,0));
+		while(!pq.isEmpty()) {
+			Edge curr = pq.poll();
+			int end = curr.end;
+			int cost = curr.cost;
+			if(dist[end]<cost) {
+				continue;
+			}
+			else {
+				dist[end] = cost;
+				for (Edge edge : edges[end]) {
+					//if문 추가 해주기
+					if(dist[edge.end]>cost+edge.cost) {
+						dist[edge.end] = cost+edge.cost;
+						pq.add(new Edge(edge.end,cost+edge.cost));
+					}
+				}
+			}
+		}
+		for(int i = 1; i<=V; i++) {
+			if(dist[i] == INF) {
+				sb.append("INF");
+			}
+			else {
+				sb.append(dist[i]);
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb);
+	}
 }
