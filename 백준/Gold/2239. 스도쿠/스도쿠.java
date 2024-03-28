b51b8ad68d1b08e8f851aa16ab98a5a33821ff40
이동열(Lee Dong Yul) <comeme0101@naver.com>
2024-03-28 09:09:10 +0900

    [Gold IV] Title: 스도쿠, Time: 552 ms, Memory: 15948 KB -BaekjoonHub

--- 백준/Gold/2239. 스도쿠/스도쿠.java
+++ 백준/Gold/2239. 스도쿠/스도쿠.java
@@ -10,7 +10,7 @@ public class Main {
     static int[][] board;
 
     private static void sol(int depth, int x, int y){
-        if(depth>81){
+    	if(depth>81){
             StringBuilder sb = new StringBuilder();
             for (int i = 0; i < 9; i++) {
                 for (int j = 0; j < 9; j++) {
@@ -30,25 +30,24 @@ public class Main {
             sol(depth+1,nx,ny);
         }
         else{
-            List<Integer> list = check(x,y);
-            for(Integer i : list){
-                board[x][y] = i;
-                sol(depth+1,nx,ny);
-                board[x][y] = 0;
+            for(int i = 1; i<=9; i++) {
+            	if(check(x, y, i)) {
+            		board[x][y] = i;
+            		sol(depth+1,nx,ny);
+            		board[x][y] = 0;
+            	}
             }
         }
     }
 
-    //해당 점을 채울 수 있는 체크, 채울 수 있으면 채울 수 있는 수 반환
-    private static List<Integer> check(int x, int y) {
-        boolean[] arr = new boolean[9];
+    private static boolean check(int x, int y, int val) {
         //가로, 세로 체크
         for (int i = 0; i < 9; i++) {
-            if (board[x][i] != 0) {
-                arr[board[x][i] - 1] = true;
+            if (board[x][i] == val) {
+                return false;
             }
-            if (board[i][y] != 0) {
-                arr[board[i][y] - 1] = true;
+            if (board[i][y] == val) {
+            	return false;
             }
         }
         //네모 체크
@@ -56,18 +55,12 @@ public class Main {
         int startY = (y / 3) * 3;
         for (int i = startX; i < startX + 3; i++) {
             for (int j = startY; j < startY + 3; j++) {
-                if(board[i][j]!=0){
-                    arr[board[i][j]-1] = true;
+                if(board[i][j]==val){
+                	return false;
                 }
             }
         }
-        List<Integer> list = new ArrayList<>();
-        for(int i = 0; i<9; i++){
-            if(!arr[i]){
-                list.add(i+1);
-            }
-        }
-        return list;
+        return true;
     }
 
     public static void main(String[] args) throws IOException {
