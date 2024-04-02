aaf55c3a9f81282a51c7d7d8fa214681a81e8914
이동열(Lee Dong Yul) <comeme0101@naver.com>
2024-04-02 23:11:33 +0900

    [Silver V] Title: 피보나치 수 4, Time: 124 ms, Memory: 18044 KB -BaekjoonHub

---
+++ 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        BigInteger[] dp = new BigInteger[10001];
+        dp[0] = BigInteger.ZERO;
+        dp[1] = BigInteger.ONE;
+        for(int i =2; i<=n; i++){
+            dp[i] = dp[i-1].add(dp[i-2]);
+        }
+        System.out.println(dp[n]);
+    }
+
+}
