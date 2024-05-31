9bada5c99aa8edd8ba17d1f43bb0abf2fafadbd5
이동열(Lee Dong Yul) <comeme0101@naver.com>
2024-06-01 00:53:31 +0900

    [Gold IV] Title: 부분합, Time: 220 ms, Memory: 22424 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int s = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[n+1];
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int start = 0;
+		int end = 0;
+		int len = Integer.MAX_VALUE;
+		int sum = 0;
+		while(start <= end && end <= n) {
+			if(sum < s) {
+				sum += arr[end++];
+			} else if(sum >= s) {
+				len = Math.min(len, end-start);
+				sum -= arr[start++];
+			}  
+		}
+		System.out.println(len==Integer.MAX_VALUE ? 0 : len);
+	}
+}
