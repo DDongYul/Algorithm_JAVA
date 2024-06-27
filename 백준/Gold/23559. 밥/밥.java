a9c87f77414a60856cb56828d5d2df2df0b5dcd0
이동열(Lee Dong Yul) <comeme0101@naver.com>
2024-06-27 11:00:19 +0900

    [Gold V] Title: 밥, Time: 456 ms, Memory: 40360 KB -BaekjoonHub

---
+++ 백준/Gold/23559. 밥/밥.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+/**
+ * 다 1000원짜리 먹고 pq에서 빼가면서 가장 큰거 5000원 짜리로 바꿔주기
+ */
+public class Main {
+
+    private static class Pair implements Comparable<Pair>{
+        int x;
+        int y;
+
+        public Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public int compareTo(Pair o) {
+            return (o.x-o.y)-(x-y);    //5000원짜리와 1000원짜리의 차 기준으로 비교
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
+        int cnt = 1000*N; //처음에 모두 1000원짜리 먹었다고 생각
+        int answer = 0;
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            pq.add(new Pair(x,y));
+            answer += y;
+        }
+        while (cnt<X && !pq.isEmpty()){
+            Pair p = pq.poll();
+            if(p.y<p.x){
+                cnt+=4000;
+                if(cnt>X) break;
+                answer+=p.x;
+                answer-=p.y;
+            }
+        }
+        System.out.println(answer);
+    }
+}
