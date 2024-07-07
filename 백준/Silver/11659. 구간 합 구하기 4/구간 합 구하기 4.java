6805038a0343ff5e0a092c7ebc6db4365fb06786
이동열(Lee Dong Yul) <comeme0101@naver.com>
2024-07-07 15:40:22 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 508 ms, Memory: 56432 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -4,30 +4,25 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int[] arr = new int[N];
-		st = new StringTokenizer(br.readLine());
-		arr[0] = Integer.parseInt(st.nextToken());
-		for(int i = 1; i<N; i++) {
-			arr[i] = arr[i-1] + Integer.parseInt(st.nextToken());
-		}
-		for(int i = 0; i<M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			if(a==1) {
-				sb.append(arr[b-1]).append("\n");			
-			}
-			else {
-				int answer = arr[b-1] - arr[a-2];
-				sb.append(answer).append("\n");		
-			}
-		}
-		System.out.println(sb);
-	}
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N+1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <=N; i++) {
+            arr[i] = arr[i-1] + Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            sb.append(arr[end] - arr[start-1]).append("\n");
+        }
+        System.out.println(sb);
+    }
 }
