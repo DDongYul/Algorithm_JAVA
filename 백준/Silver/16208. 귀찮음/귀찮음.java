6481821acccafdda1e48441a0d571ad580dcc259
이동열(Lee Dong Yul) <comeme0101@naver.com>
2024-11-01 16:52:35 +0900

    [Silver V] Title: 귀찮음, Time: 388 ms, Memory: 59372 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Silver/16208. 귀찮음/귀찮음.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        int[] sumArr = new int[N];
+        sumArr[0] = arr[0];
+        for (int i = 1; i < N; i++) {
+            sumArr[i] = sumArr[i - 1] + arr[i];
+        }
+        long answer = 0;
+        for (int i = 0; i < N; i++) {
+            answer += arr[i] * (sumArr[N-1]-sumArr[i]);
+        }
+        System.out.println(answer);
+    }
+
+}
