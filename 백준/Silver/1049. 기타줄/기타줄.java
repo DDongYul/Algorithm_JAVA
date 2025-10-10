4972e5098a67245ddbff8d6ca6746fc0960dec12
이동열(Lee Dong Yul) <comeme0101@naver.com>
2025-10-10 23:55:03 +0900

    [Silver IV] Title: 기타줄, Time: 68 ms, Memory: 11592 KB -BaekjoonHub

--- 백준/Silver/1049. 기타줄/기타줄.java
+++ 백준/Silver/1049. 기타줄/기타줄.java
@@ -1,32 +1,29 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+        
+        int min6 = 1000;
+        int min1 = 1000;
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-
-        int price1=1001;
-        int price2=1001;
-
-        for(int i=0;i<M;i++){
-            st = new StringTokenizer(br.readLine());
-            price1 = Math.min(price1, Integer.parseInt(st.nextToken()));
-            price2 = Math.min(price2, Integer.parseInt(st.nextToken()));
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			min6 = Math.min(min6, a);
+            min1 = Math.min(min1, b);
+		}
+        
+        if(min1*6<min6){
+            min6 = min1*6;
         }
-
-        if(price2*6<price1){
-            System.out.println(price2*N);
-            return;
-        }
-
-        int total = price1*(N/6);
-        total += Math.min(N%6 * price2, price1);
-        System.out.println(total);
-    }
+        
+        int answer = (N/6)*min6 + Math.min((N%6)*min1, min6);
+		System.out.println(answer);
+	}
 }
